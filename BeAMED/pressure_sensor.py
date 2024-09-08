149c734b55dde7b205f09d3f91900ef79b44dbf4
Seth <sgerow95@gmail.com>
2024-09-08 19:56:43 -0400

    Update pressure_sensor.py
    Slightly modified logic and improved GUI

--- BeAMED/pressure_sensor.py
+++ BeAMED/pressure_sensor.py
@@ -18,11 +18,7 @@ matplotlib.use('TkAgg')
 rm = pyvisa.ResourceManager()
 
 #daq ai0 = pressure
-
 #ao0 = valve 5v
-
-
-
 
 root = tk.Tk()
 
@@ -30,25 +26,28 @@ pressure = tk.IntVar()
 target_min_pressure = tk.IntVar()
 target_exp_pressure = tk.IntVar()
 
+pressure_label = tk.Label(text="Pressure").pack(anchor='w')
 pressure_box = tk.Spinbox(textvariable=pressure,
                           from_=0,
-                          to=10,)
-pressure_box.pack()
+                          to=1000,)
+pressure_box.pack(anchor='e')
+target_min_label = tk.Label(text="Target Minimum Pressure").pack(anchor='w')
 target_min_pressure_box = tk.Spinbox(textvariable=target_min_pressure,
                           from_=0,
-                          to=10,)
-target_min_pressure_box.pack()
+                          to=1000,)
+target_min_pressure_box.pack(anchor='e')
+target_exp_label = tk.Label(text="Target Experiment Pressure").pack(anchor="w")
 target_exp_pressure_box = tk.Spinbox(textvariable=target_exp_pressure,
                           from_=0,
-                          to=10,)
-target_exp_pressure_box.pack()
+                          to=1000,)
+target_exp_pressure_box.pack(anchor='e')
 
 
 
 def run():
-    pressure.set(10)
-    target_min_pressure.set(1)
-    target_exp_pressure.set(5)
+    target_min_pressure.set(100)
+    target_exp_pressure.set(700)
+    global run_bool 
     run_bool = True
 
     gas = "gas"
@@ -67,82 +66,63 @@ def run():
         ao_channel1 = task.ao_channels.add_ao_voltage_chan("NI_DAQ/ao1", min_val=0, max_val=5)
         #task.write(5,auto_start=True, timeout=10)
         #time.sleep(2)
-        
+
         #task.stop()
-        while run:
-            match state:
-                case 0:
-                    match gas:
-                        case "air":
-                            print("air")
+        print('pressure start')
+        pressure_thread.start()
+        time.sleep(1)
+        print('while bool start')
+        if gas != "air":
+            while run_bool:
+                match state:
+                    case 0:
+                        print("case 0")
+                        if pressure.get() > target_min_pressure.get():
                             task.write([[0],[0]],auto_start=True, timeout=10)
-                        case "gas":
-                            #print("gas")
-                            if pressure.get() > target_min_pressure.get():
-                                task.write([[0],[0]],auto_start=True, timeout=10)
-                            else:
-                                state = 1
-                case 1:
-                    if pressure.get() < target_exp_pressure.get():
-                        task.write([[5],[3]],
-                                   auto_start=True, timeout=10)
-                    else:
-                        task.write([[0],[0]],auto_start=True, timeout=10)
-                        state = 2
-                case 2:
-                    print("begin dischage")
-                    time.sleep(2)
-                    case = 0
-                    break
+                        else:
+                            state = 1
+                    case 1:
+                        print("case 1")
+                        if pressure.get() < target_exp_pressure.get():
+                            task.write([[5],[3]],auto_start=True, timeout=10)
+                        else:
+                            task.write([[0],[0]],auto_start=True, timeout=10)
+                            state = 2
+                    case 2:
+                        print("case 2")
+                        print("begin dischage")
+                        time.sleep(2)
+                        case = 0
+                        run_bool = False
+                        break
 
+
+def read_pressure():
+    with nidaqmx.Task() as read:
+        ai_channel = read.ai_channels.add_ai_voltage_chan("NI_DAQ/ai0", min_val=1, max_val=8, terminal_config=TerminalConfiguration.RSE)
+        read.timing.cfg_samp_clk_timing(rate=1000, sample_mode=AcquisitionType.FINITE, samps_per_chan=100)
+        
+        while run_bool:
+            pressure_sensor_voltage = np.array(read.read(100))
+            unfiltered_avg = np.median(pressure_sensor_voltage)
+            true_pressure = 10**(unfiltered_avg - 5)
+            root.after(1, lambda: pressure.set(true_pressure))
+
+
+def clean_exit():
+    rm.close()
+    root.destroy()
+
+root.protocol('WM_DELETE_WINDOW', clean_exit) 
+
+pressure_thread = Thread(target=read_pressure, daemon=True)
+print("run init")
 run_experiment = Thread(target=run, daemon=True)
-
+print("run start")
 run_experiment.start()
 root.mainloop()
 
                          
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
