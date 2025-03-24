ea4fae58f17a616814a2fd54923a63d7bb54d13c
Seth <sgerow95@gmail.com>
2025-03-24 15:08:06 -0400

    Updates transistorcontrol.py
    changes color of the output buttons and removes text labels on the buttons. Adds logic to avoid accidental outputs

--- BeAMED/BeAMED_Packet/transistorcontrol.py
+++ BeAMED/BeAMED_Packet/transistorcontrol.py
@@ -11,10 +11,10 @@ class Experiment():
         self.LED_ON = tk.PhotoImage(width=13, height=13)
         self.LED_OFF = tk.PhotoImage(width=13, height=13)
         self.LED_ON.put(("lime"), to=(0,0,12,12))
-        self.LED_OFF.put(("green"), to=(0,0,12,12))
+        self.LED_OFF.put(("red"), to=(0,0,12,12))
         self.DAQ_PIN = tk.PhotoImage(file='./BeAMED/DAQ_Pinout.PNG')
 
-        daq_line_labels = ['/0', '/ai0', '/ai4', '/0', '/ai1', '/ai5', '/0', '/ai2', '/ai6', '/0', '/ai3', '/ai7', '/0', '/ao0', '/ao1', '/0',
+        self.daq_line_labels = ['/0', '/ai0', '/ai4', '/0', '/ai1', '/ai5', '/0', '/ai2', '/ai6', '/0', '/ai3', '/ai7', '/0', '/ao0', '/ao1', '/0',
                            '/port0/line0', '/port0/line1', '/port0/line2', '/port0/line3', '/port0/line4', '/port0/line5', '/port0/line6', '/port0/line7',
                            '/port1/line0', '/port1/line1', '/port1/line2', '/port1/line3', '/port2/line0', '/0', '/0', '/0']
         analog_output_row = 0
@@ -27,25 +27,23 @@ class Experiment():
 
         self.output_bool_vars = [tk.BooleanVar() for _ in range(32)]
         self.output_buttons = [tk.Checkbutton(self.DAQ_Frame, 
-                        text = daq_line_labels[i],
                         image = self.LED_OFF, 
                         selectimage = self.LED_ON, 
                         indicatoron = False, 
                         onvalue = True, 
                         offvalue = False, 
                         variable = self.output_bool_vars[i],
-                        state = "disabled" if daq_line_labels[i]=='/0' else "normal",
+                        state = "disabled" if self.daq_line_labels[i]=='/0' else "normal",
                         command = lambda j=i: pin_output(j),
                         borderwidth=0) for i in range(32)]
         self.enable_var = [tk.IntVar() for _ in range(32)]
         self.enable_checkboxes = [tk.Checkbutton(self.DAQ_Frame,
-                                                 text = daq_line_labels[i],
                                                   variable = self.enable_var[i],
                                                   onvalue = 1,
                                                   offvalue = 0,
                                                   highlightthickness=0,
                                                   bd=0,
-                                                  state = "disabled" if daq_line_labels[i]=='/0' else "normal",
+                                                  state = "disabled" if self.daq_line_labels[i]=='/0' else "normal",
                                                   command = lambda j=i: pin_enable(j),
                                                   ) for i in range(32)]
         start_y = 99
@@ -57,10 +55,21 @@ class Experiment():
             start_y += 16
         
         def pin_output(i):
-            print(f"{self.output_buttons[i]['text']} output started")
+            if self.enable_var[i].get() == False:
+                print(f"ERROR: output for {self.daq_line_labels[i]} not enabled.")
+                self.output_bool_vars[i].set(0)
+            else:
+                if self.output_bool_vars[i].get() == 1:
+                    print(f"{self.daq_line_labels[i]} output started")
+                elif self.output_bool_vars[i].get() ==0:
+                    print(f"{self.daq_line_labels[i]} output stopped")
         
         def pin_enable(i):
-            print(f"{self.enable_checkboxes[i]['text']} enabled")
+            if self.enable_var[i].get() == False:
+                self.output_bool_vars[i].set(0)
+                print(f"{self.daq_line_labels[i]} disabled")
+            elif self.enable_var[i].get() == True:
+                print(f"{self.daq_line_labels[i]} enabled")
         
 
 if __name__ == "__main__":
