fa3c34f86f12b89db95b98219a205be831910180
Seth <sgerow95@gmail.com>
2026-06-03 11:26:06 -0400

    PID Controller
    Adds PID controller.ipynb for theoretical planning of MFC controller
    Adds PID control to MKSGE50AMFC.
    Adds snaccess.ipynb for controlling synaccess controlled power supply
    Adds Installing Python Lesson and Updates Tuples lesson

--- BeAMED/Test Scripts/MKSGE50AMFC.py
+++ BeAMED/Test Scripts/MKSGE50AMFC.py
@@ -27,7 +27,9 @@ class MFCWindow(tk.Tk):
         self.tkLock = Lock()
 
         self.measure = Event()
+        self.pid = Event()
         self.stop = Event()
+        self.stoppid = Event()
 
         #tasks for controlling inputs and outputs
         self.ao_task = nidaqmx.Task()
@@ -55,22 +57,26 @@ class MFCWindow(tk.Tk):
         self.figure_canvas = FigureCanvasTkAgg(figure, self.plotFrame)
         NavigationToolbar2Tk(self.figure_canvas,self.plotFrame).pack(side='top')
         self.axes = figure.add_subplot()
-        self.axes.set_title('MFC I/O Plot')
-        self.axes.set_ylabel('Voltage')
-        self.axes.set_xlabel('Time')
+        #self.axes.set_title('MFC I/O Plot')
+        self.axes.set_ylabel('Pressure (Torr)')
+        self.axes.set_xlabel('Time (s)')
+
+        self.axes2 = self.axes.twinx()
+        self.axes2.set_ylabel('Voltage (V)')
+        figure.tight_layout()
         self.figure_canvas.get_tk_widget().pack(side='top')
 
         self.variableFrame = tk.LabelFrame(self, text="GE50A Control")
         self.variableFrame.grid(column=1, row=0)
         self.tksetPointInput = tk.DoubleVar()
         
-        tk.Label(self.variableFrame, text = "Set Point Input (0-5 VDC)").grid(row=0,column=0)
+        tk.Label(self.variableFrame, text = "Flow Point Input (0-5 VDC)").grid(row=0,column=0)
         tk.Spinbox(self.variableFrame,
                         from_=0,
                         to = 5,
                         textvariable=self.tksetPointInput
                         ).grid(row=1, column=0)
-        '''
+        
         self.tkmkspressure = tk.DoubleVar()
         tk.Label(self.variableFrame, text="MKS Pressure").grid(row=2,column=0)
         tk.Spinbox(self.variableFrame,
@@ -92,7 +98,7 @@ class MFCWindow(tk.Tk):
                         to = 5,
                         textvariable=self.tkpressure
                         ).grid(row=7, column=0)
-        '''
+        
         self.tkgas = tk.StringVar()
         tk.Label(self.variableFrame, text="Gas").grid(row=8,column=0)
         tk.Spinbox(self.variableFrame,
@@ -106,6 +112,29 @@ class MFCWindow(tk.Tk):
         tk.Button(self.variableFrame, text='Pump Chamber',command=lambda: self.control_valve('pump')).grid(row=13,column=0)
         tk.Button(self.variableFrame, text='Close Valves',command=lambda: self.control_valve('stop')).grid(row=14,column=0)
 
+        self.tkpressureSetPoint = tk.DoubleVar(value=1)
+        tk.Label(self.variableFrame, text = "Pressure Set Point (Torr)").grid(row=0, column=1)
+        tk.Spinbox(self.variableFrame,
+                   from_=0,
+                   to=10,
+                   textvariable=self.tkpressureSetPoint).grid(row=1,column=1)
+        
+        self.tkKp = tk.DoubleVar(value=0.115*0.6)
+        self.tkKi = tk.DoubleVar(value=0.5*0.4)
+        self.tkKd = tk.DoubleVar(value=0.4/8)
+        tk.Label(self.variableFrame, text="K_p").grid(row=2,column=1)
+        tk.Spinbox(self.variableFrame,
+                   textvariable=self.tkKp).grid(row=3,column=1)
+        tk.Label(self.variableFrame, text="K_i").grid(row=4,column=1)
+        tk.Spinbox(self.variableFrame,
+                   textvariable=self.tkKi).grid(row=5,column=1)
+        tk.Label(self.variableFrame, text="K_d").grid(row=6,column=1)
+        tk.Spinbox(self.variableFrame,
+                   textvariable=self.tkKd).grid(row=7,column=1)
+        
+        tk.Button(self.variableFrame, text='Enable Control',command=lambda:self.start_pid()).grid(row=8, column=1)
+        tk.Button(self.variableFrame, text='Disable Control',command=lambda:self.stop_pid()).grid(row=9, column=1)
+
         #making data storage arrays
         self.dataframe = pd.DataFrame()
         self.flowSignalVoltage = []
@@ -113,6 +142,7 @@ class MFCWindow(tk.Tk):
         self.MKSvoltage = []
         self.setPointVoltage = []
         self.time = []
+        self.starttime = 0
 
         self.pressure_min = 0.11 #Torr #MKS Sensor
         self.pressure_max = 10 #Torr #MKS Sensor
@@ -128,6 +158,27 @@ class MFCWindow(tk.Tk):
         self.ao_task.close()
         self.do_task.close()
         self.destroy()
+
+    def pid_controller(self, setpoint, pv, kp, ki, kd, prev_error, integral, dt):
+        volume = V = 45.30695
+        error = setpoint - pv
+        integral += error * dt
+        derivative = (error - prev_error) / dt
+        control = kp * error + ki *integral + kd * derivative
+        control_mod = (control*V)*(1.333224)*(1/0.0168875)*(5/100) #V
+        control_clamp = max(0.0, min((5.0, control_mod)))
+        return control_clamp, error, integral
+    
+    def start_pid(self):
+        if self.pid.is_set():
+            pass
+        else:
+            self.pid.set()
+        self.error = 0
+        self.integral = 0
+
+    def stop_pid(self):
+        self.pid.clear()
 
     def start_measure(self):
         self.stop.clear()
@@ -147,20 +198,28 @@ class MFCWindow(tk.Tk):
             pass
         else:
             self.measure.set()
-        start_time = time.time()
+        self.starttime = time.time()
         while(self.measure.is_set()):
             with self.tkLock:
                 setpoint = float(self.tksetPointInput.get())
+                if self.pid.is_set():
+                    pressure_setpoint = float(self.tkpressureSetPoint.get())
+                    kp = float(self.tkKp.get())
+                    ki = float(self.tkKi.get())
+                    kd = float(self.tkKd.get())
             with self.daqLock:
                 kjlpressure_voltage, mkspressure_voltage, flowSignal_voltage = self.ai_task.read(10) #, flowSignal_voltage
                 flowSignal_unfiltered_avg = np.median(flowSignal_voltage)
                 kjlpressure_unfiltered_avg = np.median(kjlpressure_voltage)
                 mkspressure_unfiltered_avg = np.median(mkspressure_voltage)
-                self.ao_task.write(setpoint, auto_start = True, timeout=1)
+                read_time = time.time()
+                # if len(self.time)>=1:
+                #     print(read_time-self.time[-1])
             match self.tkgas.get():
                 case 'N2':
                     true_pressure = 10**(kjlpressure_unfiltered_avg - 5)
                     new_true_pressure = (mkspressure_unfiltered_avg/10)*(self.pressure_max-self.pressure_min)+self.pressure_min
+
                 case 'Ar':
                     print("ar")
                     print(kjlpressure_unfiltered_avg)
@@ -169,22 +228,23 @@ class MFCWindow(tk.Tk):
                     print(ar2_pressure)
                     print(true_pressure)
 
-            #with self.tkLock:
-                #self.after(1, self.tkpressure.set(true_pressure))
-                #self.after(1, self.tkflowSignalOutput.set(flowSignal_unfiltered_avg))
-                #self.after(1, self.tkmkspressure.set(new_true_pressure))
+            if self.pid.is_set() and len(self.time)>=1:
+                control, self.error, self.integral = self.pid_controller(pressure_setpoint, new_true_pressure, kp, ki, kd, self.error, self.integral, read_time-self.time[-1])
+            else:
+                control = setpoint
+            with self.daqLock:
+                self.ao_task.write(control, auto_start = True, timeout=1)
+            with self.tkLock:
+                self.after(0, self.tksetPointInput.set(control))
+                self.after(0, self.tkpressure.set(true_pressure))
+                self.after(0, self.tkflowSignalOutput.set(flowSignal_unfiltered_avg))
+                self.after(0, self.tkmkspressure.set(new_true_pressure))
             self.KJLvoltage.append(true_pressure)
             self.MKSvoltage.append(new_true_pressure)
             self.time.append(time.time())
             self.setPointVoltage.append(setpoint)
             self.flowSignalVoltage.append(flowSignal_unfiltered_avg)
 
-            if len(self.time) > 600:
-                self.KJLvoltage.pop(0)
-                self.MKSvoltage.pop(0)
-                self.time.pop(0)
-                self.setPointVoltage.pop(0)
-                self.flowSignalVoltage.pop(0)
             self.update_plot(self.time[0])
             if self.stop.is_set():
                 self.measure.clear()
@@ -192,22 +252,34 @@ class MFCWindow(tk.Tk):
             
     def update_plot(self, start_time):
         self.axes.clear()
-        time_axis = np.array(self.time)-start_time
-        kjlpressure = np.array(self.KJLvoltage)
-        set_point = np.array(self.setPointVoltage)
-        flow_signal = np.array(self.flowSignalVoltage)
-        mkspressure = np.array(self.MKSvoltage)
-
-        self.axes.plot(time_axis, kjlpressure, label = "KJL Voltage Output (V) [ai0]")
-        self.axes.plot(time_axis, mkspressure, label = "MKS Voltage Output (V) [ai1]")
-        #self.axes.plot(time_axis, flow_signal, label = "MKS Flow Signal Voltage Output (V) [ai3]")
-        #self.axes.plot(time_axis, set_point, label = "Set Point Input (V) [ao1]")
-        self.axes.set_title('Discharge Plot')
+        self.axes2.clear()
+        if len(self.time) >= 600:
+            time_axis = (np.array(self.time)-start_time)[-600:]
+            kjlpressure = np.array(self.KJLvoltage)[-600:]
+            set_point = np.array(self.setPointVoltage)[-600:]
+            flow_signal = np.array(self.flowSignalVoltage)[-600:]
+            mkspressure = np.array(self.MKSvoltage)[-600:]
+        else:
+            time_axis = (np.array(self.time)-start_time)
+            kjlpressure = np.array(self.KJLvoltage)
+            set_point = np.array(self.setPointVoltage)
+            flow_signal = np.array(self.flowSignalVoltage)
+            mkspressure = np.array(self.MKSvoltage)
+        self.axes.plot(time_axis, kjlpressure, label = "KJL Pressure (Torr) [ai0]")
+        self.axes.plot(time_axis, mkspressure, label = "MKS Pressure (Torr) [ai1]")
+        self.axes2.plot(time_axis, flow_signal, label = "MKS Flow Signal Output (V) [ai3]", color='xkcd:red')
+        self.axes2.plot(time_axis, set_point, label = "Set Point Input (V) [ao1]", color = 'xkcd:green')
+        self.axes.set_ylim(top=max(kjlpressure)*1.5)
+        self.axes2.set_ylim((-0.25,5.25))
+        #self.axes.set_title('MFC I/O Plot')
         self.axes.set_ylabel('Pressure (Torr)')
+        self.axes2.set_ylabel('Voltage (V)')
         self.axes.set_xlabel('Time (s)')
         #self.axes.set_xlim(0, 30)
-        self.axes.legend()
-
+        self.axes2.legend(loc='upper center', bbox_to_anchor=(0.5,1.05), ncol=2, fancybox=True, shadow=True)
+        self.axes.legend(loc='upper center', bbox_to_anchor=(0.5,0.98), ncol=2, fancybox=True, shadow=True)
+        self.axes.spines['top'].set_visible(False)
+        self.axes2.spines['top'].set_visible(False)
         self.figure_canvas.draw()
     
     def control_valve(self, arg:str):
